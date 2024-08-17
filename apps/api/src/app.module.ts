f25feb3d40a760c60833a7c08368d042cce9f789
dbee <dev.beecodes@gmail.com>
2024-08-16 22:21:56 -0500

    updated

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,13 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { TestControllerController } from './test-controller/test-controller.controller';
+import { TestServiceService } from './test-service/test-service.service';
+import { TestModuleModule } from './test-module/test-module.module';
+
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../', 'client/dist'),
     }),
+    UsersModule,
+    TestModuleModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TestControllerController],
+  providers: [AppService, TestServiceService],
 })
 export class AppModule {}
