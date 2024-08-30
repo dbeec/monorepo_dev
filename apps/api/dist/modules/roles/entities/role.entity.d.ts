ebc10c85d4c8cb6369ddd95ac8fdfdf2a7cdc4f7
dbee <dev.beecodes@gmail.com>
2024-08-29 22:45:25 -0500

    update(merge apilocal/main)

--- apps/api/dist/modules/roles/entities/role.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from 'src/modules/users/entities/user.entity';
-export declare class Role {
-    roleId: number;
-    name_rol: string;
-    user: User;
-    created_at: Date;
-    updated_at: Date;
-    deleted_at: Date;
-}
