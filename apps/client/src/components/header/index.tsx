5906475d93e3dc4b188423854f6a4a1381f9b7e5
dbee <dev.beecodes@gmail.com>
2024-08-31 19:09:35 -0500

    sidebar 70%

--- apps/client/src/components/header/index.tsx
+++ apps/client/src/components/header/index.tsx
@@ -1,14 +1,26 @@
 import ProfileMenu from "../avatar-menu";
 import "./style.css";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 
-export default function Header() {
+export default function Header({
+  toggleSidebar,
+}: {
+  toggleSidebar: () => void;
+}) {
   return (
     <>
       <div className="main_header">
-        <span>Buenas tardes, Johan D.</span>
+        <div className="data_header">
+          <div className="icon_header" onClick={toggleSidebar}>
+            <MenuOpenIcon />
+          </div>
+          <span>Buenas tardes, Johan D.</span>
+        </div>
 
         {/* Profile pic */}
-        <ProfileMenu />
+        <div className="avatar">
+          <ProfileMenu />
+        </div>
       </div>
     </>
   );
